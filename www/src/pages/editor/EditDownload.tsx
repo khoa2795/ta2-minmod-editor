e8d87b6c440c95ec091d2201553944ae46b38b4e
khoa2795 <bleach2792005@gmail.com>
2025-04-05 19:29:14 +0700

    explicit conditon and newline between  import, interface/function/class definitions

--- www/src/pages/editor/EditDownload.tsx
+++ www/src/pages/editor/EditDownload.tsx
@@ -1,21 +1,24 @@
 import { DownloadOutlined } from "@ant-design/icons";
-import { Button, Space } from "antd";
+import { Button } from "antd";
 import { SERVER } from "env";
 import { downloadURI } from "misc";
 import { NormSearchArgs } from "./SearchBar";
 import { observer } from "mobx-react-lite";
+
 interface DownloadButtonProp {
   normSearchArgs: NormSearchArgs;
 }
+
 export const DownloadButton: React.FC<DownloadButtonProp> = observer(({ normSearchArgs }) => {
   const handleDownload = () => {
     let url: string;
     const queryParams = new URLSearchParams();
     queryParams.append("format", "csv");
-    if (normSearchArgs.commodity) queryParams.append("commodity", normSearchArgs.commodity.id);
-    if (normSearchArgs.depositType) queryParams.append("depositType", normSearchArgs.depositType.id);
-    if (normSearchArgs.country) queryParams.append("country", normSearchArgs.country.id);
-    if (normSearchArgs.stateOrProvince) queryParams.append("stateOrProvince", normSearchArgs.stateOrProvince.id);
+
+    if (normSearchArgs.commodity !== undefined) queryParams.append("commodity", normSearchArgs.commodity.id);
+    if (normSearchArgs.depositType !== undefined) queryParams.append("depositType", normSearchArgs.depositType.id);
+    if (normSearchArgs.country !== undefined) queryParams.append("country", normSearchArgs.country.id);
+    if (normSearchArgs.stateOrProvince !== undefined) queryParams.append("stateOrProvince", normSearchArgs.stateOrProvince.id);
 
     url = `${SERVER}/api/v1/dedup-mineral-sites?${queryParams.toString()}`;
     downloadURI(url);
