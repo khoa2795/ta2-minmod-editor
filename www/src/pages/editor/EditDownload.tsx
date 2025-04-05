004f11259d029852aed33d1001d2719e0e65ab41
khoa2795 <bleach2792005@gmail.com>
2025-04-05 14:04:29 +0700

    Fix download button

--- www/src/pages/editor/EditDownload.tsx
+++ www/src/pages/editor/EditDownload.tsx
@@ -2,18 +2,28 @@ import { DownloadOutlined } from "@ant-design/icons";
 import { Button, Space } from "antd";
 import { SERVER } from "env";
 import { downloadURI } from "misc";
-
-export const DownloadButton: React.FC = () => {
+import { NormSearchArgs } from "./SearchBar";
+import { observer } from "mobx-react-lite";
+interface DownloadButtonProp {
+  normSearchArgs: NormSearchArgs;
+}
+export const DownloadButton: React.FC<DownloadButtonProp> = observer(({ normSearchArgs }) => {
   const handleDownload = () => {
-    const url = `${SERVER}/api/v1/dedup-mineral-sites?format=csv`;
+    let url: string;
+    const queryParams = new URLSearchParams();
+    queryParams.append("format", "csv");
+    if (normSearchArgs.commodity) queryParams.append("commodity", normSearchArgs.commodity.id);
+    if (normSearchArgs.depositType) queryParams.append("depositType", normSearchArgs.depositType.id);
+    if (normSearchArgs.country) queryParams.append("country", normSearchArgs.country.id);
+    if (normSearchArgs.stateOrProvince) queryParams.append("stateOrProvince", normSearchArgs.stateOrProvince.id);
+
+    url = `${SERVER}/api/v1/dedup-mineral-sites?${queryParams.toString()}`;
     downloadURI(url);
   };
 
   return (
-    <Space>
-      <Button type="primary" icon={<DownloadOutlined />} onClick={() => handleDownload()}>
-        Download
-      </Button>
-    </Space>
+    <Button type="primary" icon={<DownloadOutlined />} onClick={() => handleDownload()}>
+      Download
+    </Button>
   );
-};
+});
