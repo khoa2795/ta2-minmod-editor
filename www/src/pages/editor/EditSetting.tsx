b8da49d8975d244bace6705c6a6e7df52f911a44
khoa2795 <bleach2792005@gmail.com>
2025-04-05 14:06:55 +0700

    Use literal type to avoid typing problem

--- www/src/pages/editor/EditSetting.tsx
+++ www/src/pages/editor/EditSetting.tsx
@@ -1,37 +1,55 @@
-import { Checkbox, Form, Space, Button } from "antd";
+import { Checkbox, Form, Space, Button, Row, Col, Modal } from "antd";
+import { observer } from "mobx-react-lite";
 import { useStores } from "models";
+import { DisplayField } from "models/SettingStore";
+interface FormField {
+  fields: DisplayField[];
+}
 
-export const AddFieldModal: React.FC = () => {
+export const AddFieldModal: React.FC = observer(() => {
   const { settingStore } = useStores();
+  const displayField: { value: DisplayField; label: string }[] = [
+    { value: "geology_info", label: "Geology Info" },
+    { value: "discover_year", label: "Discover Year" },
+    { value: "mineral_form", label: "Mineral Form" },
+  ];
   return (
-    <Form
-      onFinish={(values) => {
-        settingStore.setDisplayColumns(values.fields);
-        settingStore.hideModal();
-      }}
-    >
-      <Form.Item name="fields">
-        <Checkbox.Group style={{ display: "flex", flexDirection: "column", gap: 8 }}>
-          <Checkbox value="geology_info">Geology Info</Checkbox>
-          <Checkbox value="discover_year">Discover Year</Checkbox>
-          <Checkbox value="mineral_form">Mineral Form</Checkbox>
-        </Checkbox.Group>
-      </Form.Item>
-      <Form.Item style={{ textAlign: "left" }}>
-        <Space>
-          <Button
-            onClick={() => {
-              settingStore.resetFields();
-              settingStore.hideModal();
-            }}
-          >
-            Reset
-          </Button>
-          <Button type="primary" htmlType="submit">
-            Save
-          </Button>
-        </Space>
-      </Form.Item>
-    </Form>
+      <Modal title="Add new field" open={settingStore.isModalVisible} onCancel={() => settingStore.hideSetting()} footer={null} width="70%">
+        <Form<FormField>
+          onFinish={(values) => {
+            settingStore.setDisplayColumns(values.fields);
+            settingStore.hideSetting();
+          }}
+        >
+          <Form.Item name="fields">
+            <Checkbox.Group>
+              <Row>
+                {displayField.map(({ value, label }) => (
+                  <Col span={24} key={value}>
+                    <Checkbox key={value} value={value}>
+                      {label}
+                    </Checkbox>
+                  </Col>
+                ))}
+              </Row>
+            </Checkbox.Group>
+          </Form.Item>
+          <Form.Item style={{ textAlign: "left" }}>
+            <Space>
+              <Button
+                onClick={() => {
+                  settingStore.resetFields();
+                  settingStore.hideSetting();
+                }}
+              >
+                Reset
+              </Button>
+              <Button type="primary" htmlType="submit">
+                Save
+              </Button>
+            </Space>
+          </Form.Item>
+        </Form>
+      </Modal>
   );
-};
+});
